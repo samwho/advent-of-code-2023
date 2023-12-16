ab6804aa03c4267f4ce10b2163cde29db427c3ef
Sam Rose <hello@samwho.dev>
2023-12-16 17:07:38 +0000

    Day 14 part 2.

--- 14-2.py
+++ 14-2.py
@@ -1,5 +1,4 @@
 from typing import Literal
-from tqdm import tqdm
 
 
 RoundRock = Literal["O"]
@@ -16,7 +15,7 @@ for line in f:
     grid.append(list(line.strip()))
 
 
-def move_rock_north(grid: Grid, point: tuple[int, int]):
+def move_rock_north(point: tuple[int, int]):
     x, y = point
     while y > 0 and grid[y - 1][x] == ".":
         grid[y - 1][x] = "O"
@@ -24,7 +23,7 @@ def move_rock_north(grid: Grid, point: tuple[int, int]):
         y -= 1
 
 
-def move_rock_east(grid: Grid, point: tuple[int, int]):
+def move_rock_east(point: tuple[int, int]):
     x, y = point
     while x < len(grid[0]) - 1 and grid[y][x + 1] == ".":
         grid[y][x + 1] = "O"
@@ -32,7 +31,7 @@ def move_rock_east(grid: Grid, point: tuple[int, int]):
         x += 1
 
 
-def move_rock_south(grid: Grid, point: tuple[int, int]):
+def move_rock_south(point: tuple[int, int]):
     x, y = point
     while y < len(grid) - 1 and grid[y + 1][x] == ".":
         grid[y + 1][x] = "O"
@@ -40,7 +39,7 @@ def move_rock_south(grid: Grid, point: tuple[int, int]):
         y += 1
 
 
-def move_rock_west(grid: Grid, point: tuple[int, int]):
+def move_rock_west(point: tuple[int, int]):
     x, y = point
     while x > 0 and grid[y][x - 1] == ".":
         grid[y][x - 1] = "O"
@@ -48,94 +47,95 @@ def move_rock_west(grid: Grid, point: tuple[int, int]):
         x -= 1
 
 
-def stone_value(grid: Grid, point: tuple[int, int]) -> int:
+def stone_value(point: tuple[int, int]) -> int:
     _, y = point
     return len(grid) - y
 
 
-def tilt_north(grid: Grid):
+def tilt_north():
     for y in range(len(grid)):
         for x in range(len(grid[0])):
             if grid[y][x] == "O":
-                move_rock_north(grid, (x, y))
+                move_rock_north((x, y))
 
 
-def tilt_west(grid: Grid):
+def tilt_west():
     for y in range(len(grid)):
         for x in range(len(grid[0])):
             if grid[y][x] == "O":
-                move_rock_west(grid, (x, y))
+                move_rock_west((x, y))
 
 
-def tilt_south(grid: Grid):
+def tilt_south():
     for y in range(len(grid) - 1, -1, -1):
         for x in range(len(grid[0])):
             if grid[y][x] == "O":
-                move_rock_south(grid, (x, y))
+                move_rock_south((x, y))
 
 
-def tilt_east(grid: Grid):
+def tilt_east():
     for y in range(len(grid)):
         for x in range(len(grid[0]) - 1, -1, -1):
             if grid[y][x] == "O":
-                move_rock_east(grid, (x, y))
+                move_rock_east((x, y))
 
 
-def spin_cycle(grid: Grid):
-    tilt_north(grid)
-    tilt_west(grid)
-    tilt_south(grid)
-    tilt_east(grid)
+def spin_cycle():
+    tilt_north()
+    tilt_west()
+    tilt_south()
+    tilt_east()
 
 
-def calculate_weight(grid: Grid) -> int:
+def calculate_weight() -> int:
     weight = 0
     for y in range(len(grid)):
         for x in range(len(grid[0])):
             if grid[y][x] == "O":
-                weight += stone_value(grid, (x, y))
+                weight += stone_value((x, y))
     return weight
 
 
-def print_grid(grid: Grid):
+def print_grid():
     for row in grid:
         print("".join(row))
 
 
-def grid_to_str(grid: Grid) -> str:
-    return "\n".join("".join(row) for row in grid)
+def grid_to_str() -> str:
+    return "".join("".join(row) for row in grid)
 
 
-WEIGHTS = []
-UNIQUE_WEIGHTS = set()
-
-for i in tqdm(range(10000)):
-    spin_cycle(grid)
-    weight = calculate_weight(grid)
-    WEIGHTS.append(weight)
-    UNIQUE_WEIGHTS.add(weight)
-
-print(UNIQUE_WEIGHTS)
+def diff_strings(s1: str, s2: str):
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            print("-", end="")
+        else:
+            print("+", end="")
+    print()
 
 
-def guess_seq_len(seq, verbose=False):
-    seq_len = 1
-    initial_item = seq[0]
-    butfirst_items = seq[1:]
-    if initial_item in butfirst_items:
-        first_match_idx = butfirst_items.index(initial_item)
-        if verbose:
-            print(f'"{initial_item}" was found at index 0 and index {first_match_idx}')
-        max_seq_len = min(len(seq) - first_match_idx, first_match_idx)
-        for seq_len in range(max_seq_len, 0, -1):
-            if seq[:seq_len] == seq[first_match_idx : first_match_idx + seq_len]:
-                if verbose:
-                    print(
-                        f"A sequence length of {seq_len} was found at index {first_match_idx}"
-                    )
-                break
+start = 0
+cycle = 0
 
-    return seq_len
+SEEN = {}
 
+print("finding cycle...")
+for i in range(0, 1000000):
+    spin_cycle()
+    s = grid_to_str()
+    if s in SEEN:
+        start = SEEN[s]
+        cycle = i - SEEN[s]
+        print(f"cycle found: start {start} period {cycle}")
+        break
+    SEEN[s] = i
 
-guess_seq_len(WEIGHTS, verbose=True)
+if cycle == 0:
+    raise ValueError("No cycle found")
+
+iterations = (1000000000 - start) % cycle
+
+for i in range(iterations - 1):
+    spin_cycle()
+
+print(calculate_weight())
